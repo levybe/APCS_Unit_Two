b918300c24c00425649e6e765495124235c7a0a6
levybe <levybe@ssfs.org>
2021-10-12 12:54:33 -0400

    Finished EmailGenerator.

--- src/Distance.java
+++ src/Distance.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Distance {
     // create the distance method below. See the instructions for the signature of the method.
     public static double calculateDistance() {
-
+    return 0;
     }
     // Create your main method below. It will get the input (you will need a Scanner), call the distance
     // method, and then print the results.
--- src/EmailGenerator.java
+++ src/EmailGenerator.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class EmailGenerator {
-public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first name:");
+        String firstName = scan.nextLine();
+        System.out.println("Enter your last name:");
+        String lastName = scan.nextLine();
+        System.out.println("Enter your email provider:");
+        String emailProvider = scan.nextLine();
+        String userName = makeUserName(firstName, lastName);
+        String email = makeEmail(userName, emailProvider);
+        System.out.println(firstName + " " + lastName + ", your new email is " + email + ".");
+    }
 
-}
+    public static String makeUserName(String firstName, String lastName) {
+     String userName = (firstName.toLowerCase()).substring(0, 1) + lastName.toLowerCase() + ((int) ((Math.random()*90) + 10));
+     return userName;
+    }
 
-public static String makeUserName() {
-    int x = 1;
-    return "thing";
+    public static String makeEmail(String userName, String emailProvider) {
+        String email = userName + "@" + emailProvider;
+        return email;
     }
 
 }
